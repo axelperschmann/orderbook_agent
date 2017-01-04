5c5417a349239f246ed78df829bde29208db072b
Axel Perschmann <ax.perschmann@gmail.com>
2017-01-04 16:22:04 +0100

    added proper trading history

--- orderbook_trade_simulator.py
+++ orderbook_trade_simulator.py
@@ -4,6 +4,8 @@ from manage_orderbooks import log_mean, orderbook_preview
 from IPython.display import display
 from tqdm import tqdm
 
+PRECISION = 10
+
 class OrderbookTradingSimulator:
     
     def summarize(self, df):
@@ -21,13 +23,9 @@ class OrderbookTradingSimulator:
         display(self.sell_history)
         print("Last trade")
         display(self.last_trade)
-        if self.volume_traded != 0:
-            print("Cashflow: {} (Avg: {}, Low: {}, High: {})".format(self.cashflow,
-                                                                     self.cashflow / self.volume_traded,
-                                                                     self.lowest_price, self.highest_price))
-        else:
-            print("Cashflow: 0")
-        print("Traded: {}/{} shares".format(self.volume_traded, (self.volume_traded + self.volume_nottraded)))
+        
+        print("Last trade  : {:1.4f}/{:1.4f} shares".format(self.history.volume_traded.values[-1], self.history.VOLUME.values[-1]))
+        print("Total trades: {:1.4f}/{:1.4f} shares".format(sum(self.history.volume_traded), self.history.VOLUME.values[0]))
         
         print("Spread: {} (bid: {}, ask: {})".format(ask-bid, bid, ask))
         
@@ -41,19 +39,10 @@ class OrderbookTradingSimulator:
         
         self.purchase_history = {}  # {'705.45': 3.1721811199999999}
         self.sell_history = {}  # {'703.70': 0.001}
-
         self.last_trade = {}
-        self.volume_of_last_trade = 0
-        self.volume_of_last_trade_period = 0
-        self.volume_traded = 0
-        self.volume_nottraded = 0
-        self.cashflow = 0
-        self.highest_price = 0
-        self.lowest_price = np.inf
-    
-        self.initial_bid = None
-        self.initial_ask = None
-        self.initial_center = None
+        
+        self.history = pd.DataFrame([])
+
     
     def adjust_orderbook(self, df, verbose=False):
         assert isinstance(df, pd.DataFrame)
@@ -92,7 +81,6 @@ class OrderbookTradingSimulator:
         df.loc[df[df['Type'] == 'center'].index, 'Price'] = self.center_log
         
         # compute accumulated order volume
-        # df['VolumeAcc'].loc[df.Type=='bid'] = (df[df.Type=='bid'].Volume)[::-1].cumsum().values[::-1]
         df.loc[df.Type=='bid', 'VolumeAcc'] = (df[df.Type=='bid'].Volume)[::-1].cumsum().values[::-1]
         df.loc[df.Type=='ask', 'VolumeAcc'] = (df[df.Type=='ask'].Volume).cumsum().values
 
@@ -107,9 +95,11 @@ class OrderbookTradingSimulator:
         assert volume != 0
         assert isinstance(limit, float) or isinstance(limit, int) or not limit
         
+        info = {'cashflow': 0,
+                'volume':   0}
+        
         self.t += 1
         trade_summary = {}
-        self.volume_of_last_trade = 0
         
         if volume > 0:
             # buy from market
@@ -127,7 +117,7 @@ class OrderbookTradingSimulator:
         for pos in range(len(df)):
             order = df.iloc[pos]
             price = str(order.Price)
-            
+           
             if limit:
                 if (order_type == 'ask' and order.Price > limit) or (order_type == 'bid' and order.Price < limit):
                     # Price limit exceeded, stop trading now!
@@ -148,24 +138,24 @@ class OrderbookTradingSimulator:
                 history[price] += current_order_volume
             else:
                 history[price] = current_order_volume
-            
-            self.cashflow += current_order_volume * order.Price * order_direction
-            self.volume_traded -= current_order_volume * order_direction
-            self.volume_of_last_trade -= current_order_volume * order_direction
 
-            volume += current_order_volume * order_direction
-            if order.Price > self.highest_price:
-                self.highest_price = order.Price
-            if order.Price < self.lowest_price:
-                self.lowest_price = order.Price
+            info['cashflow'] += current_order_volume * order.Price * order_direction
+            info['volume'] -= current_order_volume * order_direction
             
+            volume += current_order_volume * order_direction
+
             if volume == 0:
                 break
         self.volume_nottraded = volume
         
         self.last_trade = trade_summary
         
-    def trade_timespan(self, orderbooks, volume, limit, verbose=True, timespan=1, must_trade=False):
+        # Round trading volume to prevent subsequent rounding issues like volume_left = -1.421085e-14 (vs. intended 0.0)
+        info['volume'] = round(info['volume'], PRECISION)
+        
+        return info
+        
+    def trade_timespan(self, orderbooks, timestamps, volume, limit, verbose=True, timespan=1, must_trade=False):
         assert isinstance(orderbooks, list)
         assert len(orderbooks)==timespan
         assert isinstance(volume, float) or isinstance(volume, int)
@@ -177,49 +167,94 @@ class OrderbookTradingSimulator:
         
         volume_traded = 0
         self.volume_of_last_trade_period = 0
+
+        info = pd.DataFrame(data={'BID': None,
+                                  'ASK': None,
+                                  'SPREAD': None,
+                                  'CENTER': None,
+                                  'TIMESPAN': timespan,
+                                  'VOLUME': volume,
+                                  'volume_traded': 0,
+                                  'volume_left': volume,
+                                  'LIMIT': limit,
+                                  'cashflow': 0,
+                                  'high': 0,
+                                  'low': np.inf,
+                                  'avg': 0,
+                                  'slippage': 0},
+                            index=[timestamps[0][:-10]])
         
-        for t in tqdm(range(timespan)):
+        for t in tqdm(range(timespan), leave=True):
             assert isinstance(orderbooks[t], pd.DataFrame)
             df = orderbooks[t].copy()
             df = self.adjust_orderbook(df)
-            # display(orderbook_preview(df,3))
-            # print("Trade:", volume, volume-self.volume_traded)
+            
+            if t == 0:
+                info.ASK = df[df.Type == 'ask'].iloc[0].Price
+                info.BID = df[df.Type == 'bid'].iloc[-1].Price
+                info.SPREAD = (info.ASK - info.BID)
+                info.CENTER = log_mean(info.ASK.values[0], info.BID.values[0])           
+
                
             if volume==0:
                 # Do nothing!
                 return df
-            elif volume > 0:
-                # buy from market
-                order_direction = -1
-            elif volume < 0:
-                # sell to market
-                order_direction = 1
                 
             if must_trade and t == timespan-1:
                 # must sell all remaining shares. Place Market Order!
-                
-                print("Run out of time (t={}). Sell remaining {:.4f}/{:.4f} shares for current market order price".format(t, volume + order_direction*volume_traded, volume))
+                print("Run out of time (t={}).\nSell remaining {:.4f}/{:.4f} shares for current market order price".format(t,
+                                                                                                  info.volume_left.values[0],
+                                                                                                  info.VOLUME.values[0]))
                 limit = None
                 
-            self.__perform_trade(df, volume + volume_traded*order_direction, limit)
-            self.volume_of_last_trade_period -= self.volume_of_last_trade * order_direction
+            # perform trade
+            trade_result = self.__perform_trade(df, info.volume_left.values[0], limit)
+           
+            info.cashflow += trade_result['cashflow']
+            info.volume_traded += trade_result['volume']
+            info.volume_left = round((info.VOLUME - info.volume_traded).values[0], PRECISION)
             
-            volume_traded -= self.volume_of_last_trade * order_direction
-
-            if abs(volume)-abs(volume_traded) == 0:
+            if len(self.last_trade) > 0:
+                current_high = max([float(x) for x in self.last_trade.keys()])
+                if current_high > info.high.values[0]:
+                    info.high = current_high
+                    
+                current_low = min([float(x) for x in self.last_trade.keys()])
+                if current_low < info.low.values[0]:
+                    info.low = current_low
+            
+            
+            if abs(info.volume_left.values[0]) == 0:
                 print("No shares left at t={}, Done!".format(t))
                 break
+                
+        info.avg = abs(info.cashflow / info.volume_traded)
+        
+        if volume > 0:
+            # buy from market
+            info.slippage = info.cashflow + info.volume_traded * info.ASK
+        elif volume < 0:
+            # sell to market
+            info.slippage = info.cashflow + info.volume_traded * info.BID
+        
+        self.history = self.history.append(info, ignore_index=False)
+        display(self.history)
 
         if verbose:
             self.summarize(df)
             
-            print("Traded {:.4f}/{:.4f} shares for {}".format(self.volume_traded, self.volume_traded+ self.volume_nottraded,
-                                                                  self.cashflow))
+            print("Traded {:.4f}/{:.4f} shares for {}, {:.4f} shares left".format(info.volume_traded.values[0],
+                                                              (info.volume_traded + info.volume_left).values[0],
+                                                              info.cashflow.values[0],
+                                                              info.volume_left.values[0]))
             
         # self.summarize(df)
         return self.adjust_orderbook(df)
         
     
+    
+    
+    ## deprecated ... delete?
     def trade(self, orderbook, volume, limit=None, verbose=True):
         assert isinstance(orderbook, pd.DataFrame)
         assert isinstance(volume, float) or isinstance(volume, int)
@@ -231,20 +266,19 @@ class OrderbookTradingSimulator:
         df = orderbook.copy()
         df = self.adjust_orderbook(df)
         
-        if not self.initial_center:
+        if not self.initial_center_log:
             # very first trade
             self.initial_ask = df[df.Type == 'ask'].iloc[0].Price
             self.initial_bid = df[df.Type == 'bid'].iloc[-1].Price
             self.initial_center_log = log_mean(self.initial_ask, self.initial_bid)
         
-        self.volume_of_last_trade = 0
         self.__perform_trade(df, volume, limit)
         
         if verbose:
             self.summarize(df)
             
-            print("Traded {:.4f}/{:.4f} shares for {}".format(self.volume_traded, self.volume_traded + self.volume_nottraded,
-                                                                  self.cashflow))
+            # print("Traded {:.4f}/{:.4f} shares for {}".format(self.volume_traded, self.volume_traded + self.volume_nottraded,
+            #                                                       self.cashflow))
             
         # self.summarize(df)
         return self.adjust_orderbook(df)
