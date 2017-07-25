44e983f6e562fe3bf38de7403e2699355e035ed5
Axel Perschmann <axel@5dlab.com>
2017-07-25 14:38:10 +0200

    collect samples

--- orderbook_agent/notebooks/collect_samples_forward.py
+++ orderbook_agent/notebooks/collect_samples_forward.py
@@ -15,6 +15,7 @@ import matplotlib.dates as mdates
 from mpl_toolkits.mplot3d import Axes3D
 from IPython.display import display
 import pickle
+from random import shuffle
 
 import sys
 sys.path.append('..')
@@ -32,29 +33,6 @@ from Runs.train_agents import trainer_BatchTree, trainer_QTable
 
 num_cores = multiprocessing.cpu_count()
 
-# ## Preprocess data
-# histfiles = [
-#     "../../../../data/history/history_2016-11_USDT_BTC.csv",
-# #     "../../../../data/history/history_2016-12_USDT_BTC.csv",
-# #     "../../../../data/history/history_2017-01_USDT_BTC.csv",
-# #     "../../../../data/history/history_2017-02_USDT_BTC.csv",
-# ]
-# # 
-# hist = load_and_preprocess_historyfiles(histfiles)
-# # 
-# hist['future15_disc'] = pd.cut(hist.future15, bins=[-np.inf, -0.005, -0.001, 0.001, 0.005, np.inf], labels=False)
-# hist['future30_disc'] = pd.cut(hist.future30, bins=[-np.inf, -0.005, -0.001, 0.001, 0.005, np.inf], labels=False)
-# hist['future45_disc'] = pd.cut(hist.future45, bins=[-np.inf, -0.005, -0.001, 0.001, 0.005, np.inf], labels=False)
-# hist['spread_disc'] = pd.cut(hist.spread, bins=[0, 1, 2, np.inf], labels=False)
-# # display(hist.iloc[1021:1025,:])
-
-
-
-# print("data_mini")
-# data = pickle.load( open( "cached_windows/tradingwindows_1611_USTD_BTC_20.p", "rb" ) )
-# for window in data:
-# 	for ob in window:
-# 		ob.features = {}
 
 # load data
 data_nov = pickle.load( open( '../cached_windows_60mins_V200/obs_2016-11_USDT_BTC_maxVol200.p', "rb" ) )
@@ -77,20 +55,34 @@ print("data_feb", len(data_feb))
 data_mar = pickle.load( open( "../cached_windows_60mins_V200/obs_2017-03_USDT_BTC_maxVol200.p", "rb" ) )
 print("data_mar", len(data_mar))
 # # # data_mar = Parallel(n_jobs=num_cores, verbose=10)(delayed(add_features_to_orderbooks)(orderbooks=window, hist=hist) for window in data_mar[:])
-# # 
+# 
 data_apr = pickle.load( open( "../cached_windows_60mins_V200/obs_2017-04_USDT_BTC_maxVol200.p", "rb" ) )
 print("data_apr", len(data_apr))
+# # data_apr = Parallel(n_jobs=num_cores, verbose=10)(delayed(add_features_to_orderbooks)(orderbooks=window, hist=hist) for window in data_apr[:])
+
+data_may = pickle.load( open( "../cached_windows_60mins_V200/obs_2017-05_USDT_BTC_maxVol200.p", "rb" ) )
+print("data_may", len(data_may))
 # # data_apr = Parallel(n_jobs=num_cores, verbose=10)(delayed(add_features_to_orderbooks)(orderbooks=window, hist=hist) for window in data_apr[:])
 
 ################
 ### SETTINGS ###
 ################
-data = data_nov + data_dec + data_jan + data_feb + data_mar + data_apr
+data = data_nov + data_dec + data_jan + data_feb + data_mar + data_apr + data_may[50:52] + data_may[100:102] + data_may[200:205] + data_may[300:315] + data_may[400:405]
+
+# data_eth_jun = pickle.load( open( "/home/axel/notebooks/orderbook_agent/orderbook_agent/cached_windows_USDTETH/obs_2017-06_USDT_ETH_maxVol1000.p", "rb" ) )
+# data_eth_jul = pickle.load( open( "/home/axel/notebooks/orderbook_agent/orderbook_agent/cached_windows_USDTETH/obs_2017-07_USDT_ETH_maxVol1000.p", "rb" ) )
+# data = data_eth_jun + data_eth_jul
+# print("len(data)", len(data))
+data = data[:int(len(data)*0.8)]
+
+shuffle(data)
+print("len(data)", len(data))
 
 T=4
 P=15
 V=70000
 consume='cash'
+state_variables = ['volume', 'time', 'level2data']
 print("T: {}, P: {}, V: {}, consume: '{}'".format(T, P, V, consume))
 
 # actions = np.linspace(-0.4, 1.0, num=15)
@@ -98,9 +90,13 @@ actions = range(-4,11)
 print("Trading windows: {} (each one: {} minutes)".format(len(data), len(data[0])))
 
 
-### QTable Agent
-agent = trainer_QTable(orderbooks=data, V=V, T=T, consume=consume, actions=[round(a, 2) for a in actions],
-                    limit_base='currAsk', vol_intervals=8,
-                    period_length=P, agent_name='QTable_1611-1704_T4_I8_VolTime',
-                    state_variables=['volume', 'time'], mode='backward')
-agent.save(path="trainedAgents/longterm_1611_1704_simulate_preceeding_trades", overwrite=True)
+print("BT-Agent")
+### BT Agent
+agent = trainer_BatchTree(orderbooks=data[:], V=V, T=T, consume=consume, actions=actions,
+                    lim_stepsize=0.1, limit_base='currAsk',
+                    period_length=P, epochs=60, agent_name='BT_Agent_shuffle',
+                    random_start=False, state_variables=state_variables, 
+                    mode='forward', retraining=24, savepath='trainedAgents/USDTETH')
+
+
+agent.save(path="trainedAgents/USDTETH", overwrite=True)
